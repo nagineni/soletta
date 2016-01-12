6c1a150cbe9564125bab485156bde71858ff73c6
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2016-01-12 19:41:16 +0200

    Node.js bindings: GPIO/AIO/PWM: Added bindings for GPIO, AIO and PWM.
    Signed-off-by: Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>

---
+++ bindings/nodejs/src/functions/gpio.cc
@@ -0,0 +1,77 @@
+extern "C" {
+#include <soletta/sol-gpio.h>
+}
+
+#include <v8.h>
+#include <node.h>
+#include <nan.h>
+
+#include "common.h"
+#include "structures/sol-js-gpio.h"
+#include "structures/handles.h"
+
+using namespace v8;
+
+NAN_METHOD(bind_sol_gpio_open) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsUint32);
+	VALIDATE_ARGUMENT_TYPE(info, 1, IsObject);
+
+	uint32_t pin;
+	sol_gpio_config config;
+	sol_gpio *gpio = NULL;
+
+	pin = info[0]->Uint32Value();
+	if (!c_sol_gpio_config(info[1]->ToObject(), &config)) {
+		printf("Unable to extract sol_gpio_config\n");
+    	return;
+	}
+
+	gpio = sol_gpio_open(pin, &config);
+	if ( gpio ) {
+		info.GetReturnValue().Set(js_sol_gpio(gpio));
+	}
+}
+
+NAN_METHOD(bind_sol_gpio_close) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_gpio *gpio = NULL;
+
+ 	if (!c_sol_gpio(Local<Array>::Cast(info[0]), &gpio)) {
+    	return;
+	}
+
+	sol_gpio_close(gpio);
+}
+
+NAN_METHOD(bind_sol_gpio_write) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsBoolean);
+
+	bool value;
+	sol_gpio *gpio = NULL;
+
+ 	if (!c_sol_gpio(Local<Array>::Cast(info[0]), &gpio)) {
+    	return;
+	}
+
+	value = info[1]->BooleanValue();
+
+	info.GetReturnValue().Set(Nan::New(sol_gpio_write(gpio, value)));
+}
+
+NAN_METHOD(bind_sol_gpio_read) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_gpio *gpio = NULL;
+
+ 	if (!c_sol_gpio(Local<Array>::Cast(info[0]), &gpio)) {
+    	return;
+	}
+
+	info.GetReturnValue().Set(Nan::New(sol_gpio_read(gpio)));
+}
---
+++ bindings/nodejs/src/functions/pwm.cc
@@ -0,0 +1,142 @@
+extern "C" {
+#include <soletta/sol-pwm.h>
+}
+
+#include <v8.h>
+#include <node.h>
+#include <nan.h>
+
+#include "common.h"
+#include "structures/sol-js-pwm.h"
+#include "structures/handles.h"
+
+using namespace v8;
+
+NAN_METHOD(bind_sol_pwm_open) {
+	VALIDATE_ARGUMENT_COUNT(info, 3);
+	VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsInt32);
+ 	VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 1, IsInt32);
+	VALIDATE_ARGUMENT_TYPE(info, 2, IsObject);
+
+	int device;
+  int channel;
+	sol_pwm_config config;
+	sol_pwm *pwm = NULL;
+
+	device = info[0]->Int32Value();
+  channel = info[1]->Int32Value();
+
+	if (!c_sol_pwm_config(info[2]->ToObject(), &config)) {
+		printf("Unable to extract sol_pwm_config\n");
+    	return;
+	}
+
+	pwm = sol_pwm_open(device, channel, &config);
+	if ( pwm ) {
+		info.GetReturnValue().Set(js_sol_pwm(pwm));
+	}
+}
+
+NAN_METHOD(bind_sol_pwm_close) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	sol_pwm_close(pwm);
+}
+
+NAN_METHOD(bind_sol_pwm_set_enabled) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsBoolean);
+
+	bool value;
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	value = info[1]->BooleanValue();
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_set_enabled(pwm, value)));
+}
+
+NAN_METHOD(bind_sol_pwm_set_period) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsUint32);
+
+	uint32_t value;
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	value = info[1]->Uint32Value();
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_set_period(pwm, value)));
+}
+
+NAN_METHOD(bind_sol_pwm_set_duty_cycle) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsUint32);
+
+	uint32_t value;
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	value = info[1]->Uint32Value();
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_set_duty_cycle(pwm, value)));
+}
+
+
+NAN_METHOD(bind_sol_pwm_get_enabled) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_get_enabled(pwm)));
+}
+
+NAN_METHOD(bind_sol_pwm_get_period) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_get_period(pwm)));
+}
+
+NAN_METHOD(bind_sol_pwm_get_duty_cycle) {
+	VALIDATE_ARGUMENT_COUNT(info, 1);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+
+	sol_pwm *pwm = NULL;
+
+ 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    	return;
+	}
+
+	info.GetReturnValue().Set(Nan::New(sol_pwm_get_duty_cycle(pwm)));
+}
