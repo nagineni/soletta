f9cc577a6002c707b5e31f2c202b95adf353e272
Gabriel Schulhof <gabriel.schulhof@intel.com>
2016-01-17 01:04:09 +0200

    Node.js bindings: Add remaining platform bindings
    Signed-off-by: Gabriel Schulhof <gabriel.schulhof@intel.com>

--- bindings/nodejs/src/functions/sol-platform-monitors.cc
+++ bindings/nodejs/src/functions/sol-platform-monitors.cc
@@ -7,14 +7,8 @@
 
 using namespace v8;
 
-#define SOL_PLATFORM_MONITOR_ADD(name, callback_type, monitor_name) \
+#define SOL_PLATFORM_MONITOR_PROCESS_ADD_RESULT(result, jsCallback, info) \
 	do { \
-		VALIDATE_ARGUMENT_COUNT(info, 1); \
-		VALIDATE_ARGUMENT_TYPE(info, 0, IsFunction); \
-\
-		Nan::Callback *jsCallback = new Nan::Callback(Local<Function>::Cast(info[0])); \
-		int result = sol_platform_add_##name##_monitor((callback_type)monitor_name, jsCallback); \
-\
 		if (result) { \
 			delete jsCallback; \
 		} else { \
@@ -26,7 +20,18 @@ using namespace v8;
 		hijack_ref(); \
 	} while(0)
 
-#define SOL_PLATFORM_MONITOR_DEL(name, callback_type, monitor_name) \
+#define SOL_PLATFORM_MONITOR_ADD(name, callback) \
+	do { \
+		VALIDATE_ARGUMENT_COUNT(info, 1); \
+		VALIDATE_ARGUMENT_TYPE(info, 0, IsFunction); \
+\
+		Nan::Callback *jsCallback = new Nan::Callback(Local<Function>::Cast(info[0])); \
+		int result = sol_platform_add_##name##_monitor((callback), jsCallback); \
+\
+		SOL_PLATFORM_MONITOR_PROCESS_ADD_RESULT(result, jsCallback, info); \
+	} while(0)
+
+#define SOL_PLATFORM_MONITOR_DEL(name, callback) \
 	do { \
 		VALIDATE_ARGUMENT_COUNT(info, 1); \
 		VALIDATE_ARGUMENT_TYPE(info, 0, IsFunction); \
@@ -37,7 +42,7 @@ using namespace v8;
 				Local<Array>::Cast(Nan::Get(info[0]->ToObject(), \
 					Nan::New("_callback").ToLocalChecked()).ToLocalChecked()))) { \
 \
-			int result = sol_platform_del_##name##_monitor((callback_type)monitor_name, jsCallback); \
+			int result = sol_platform_del_##name##_monitor((callback), jsCallback); \
 			if (!result) { \
 				delete jsCallback; \
 			} \
@@ -46,13 +51,13 @@ using namespace v8;
 		} \
 	} while(0)
 
-void defaultStringMonitor(void *data, const char *the_string) {
+static void defaultStringMonitor(void *data, const char *the_string) {
 	Nan::HandleScope scope;
 	Local<Value> jsCallbackArguments[1] = {Nan::New(the_string).ToLocalChecked()};
 	((Nan::Callback *)data)->Call(1, jsCallbackArguments);
 }
 
-void defaultIntStringMonitor(void *data, int the_int, const char *the_string) {
+static void defaultIntStringMonitor(void *data, int the_int, const char *the_string) {
 	Nan::HandleScope scope;
 	Local<Value> jsCallbackArguments[2] = {
 		Nan::New(the_int),
@@ -61,38 +66,102 @@ void defaultIntStringMonitor(void *data, int the_int, const char *the_string) {
 	((Nan::Callback *)data)->Call(2, jsCallbackArguments);
 }
 
+static void defaultStringIntMonitor(void *data, const char *the_string, int the_int) {
+	defaultIntStringMonitor(data, the_int, the_string);
+}
+
+static void defaultIntMonitor(void *data, int the_int) {
+	Nan::HandleScope scope;
+	Local<Value> jsCallbackArguments[1] = {Nan::New(the_int)};
+	((Nan::Callback *)data)->Call(1, jsCallbackArguments);
+}
+
+static void defaultTimestampMonitor(void *data, int64_t the_timestamp) {
+	Nan::HandleScope scope;
+	Local<Value> jsCallbackArguments[1] = {Nan::New<Date>((double)the_timestamp).ToLocalChecked()};
+	((Nan::Callback *)data)->Call(1, jsCallbackArguments);
+}
+
 NAN_METHOD(bind_sol_platform_add_hostname_monitor) {
 	SOL_PLATFORM_MONITOR_ADD(hostname,
-		void(*)(void *, const char *),
-		defaultStringMonitor);
+		(void(*)(void *, const char *))defaultStringMonitor);
 }
 
 NAN_METHOD(bind_sol_platform_del_hostname_monitor) {
 	SOL_PLATFORM_MONITOR_DEL(hostname,
-		void(*)(void *, const char *),
-		defaultStringMonitor);
+		(void(*)(void *, const char *))defaultStringMonitor);
 }
 
 NAN_METHOD(bind_sol_platform_add_timezone_monitor) {
 	SOL_PLATFORM_MONITOR_ADD(timezone,
-		void(*)(void *, const char *),
-		defaultStringMonitor);
+		(void(*)(void *, const char *))defaultStringMonitor);
 }
 
 NAN_METHOD(bind_sol_platform_del_timezone_monitor) {
 	SOL_PLATFORM_MONITOR_DEL(timezone,
-		void(*)(void *, const char *),
-		defaultStringMonitor);
+		(void(*)(void *, const char *))defaultStringMonitor);
 }
 
 NAN_METHOD(bind_sol_platform_add_locale_monitor) {
 	SOL_PLATFORM_MONITOR_ADD(locale,
-		void(*)(void *, enum sol_platform_locale_category, const char *),
-		defaultIntStringMonitor);
+		(void(*)(void *, enum sol_platform_locale_category, const char *))defaultIntStringMonitor);
 }
 
 NAN_METHOD(bind_sol_platform_del_locale_monitor) {
 	SOL_PLATFORM_MONITOR_DEL(locale,
-		void(*)(void *, enum sol_platform_locale_category, const char *),
-		defaultIntStringMonitor);
+		(void(*)(void *, enum sol_platform_locale_category, const char *))defaultIntStringMonitor);
+}
+
+NAN_METHOD(bind_sol_platform_add_service_monitor) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsFunction);
+	VALIDATE_ARGUMENT_TYPE(info, 1, IsString);
+
+	Nan::Callback *jsCallback = new Nan::Callback(Local<Function>::Cast(info[0]));
+	int result = sol_platform_add_service_monitor(
+		(void(*)(void *, const char *, enum sol_platform_service_state))defaultStringIntMonitor,
+		(const char *)*String::Utf8Value(info[1]),
+		jsCallback);
+
+	SOL_PLATFORM_MONITOR_PROCESS_ADD_RESULT(result, jsCallback, info);
+}
+
+NAN_METHOD(bind_sol_platform_del_service_monitor) {
+	VALIDATE_ARGUMENT_COUNT(info, 2);
+	VALIDATE_ARGUMENT_TYPE(info, 0, IsFunction);
+	VALIDATE_ARGUMENT_TYPE(info, 1, IsString);
+
+	Nan::Callback *jsCallback = 0;
+	if (fillCArrayFromJSArray((unsigned char *)&jsCallback,
+			sizeof(Nan::Callback *),
+			Local<Array>::Cast(Nan::Get(info[0]->ToObject(),
+				Nan::New("_callback").ToLocalChecked()).ToLocalChecked()))) {
+
+		int result = sol_platform_del_service_monitor(
+			(void(*)(void *, const char *, enum sol_platform_service_state))defaultStringIntMonitor,
+			(const char *)*String::Utf8Value(info[1]),
+			jsCallback);
+		if (!result) {
+			delete jsCallback;
+		}
+		info.GetReturnValue().Set(Nan::New(result));
+	}
+}
+
+NAN_METHOD(bind_sol_platform_add_state_monitor) {
+	SOL_PLATFORM_MONITOR_ADD(state,
+		(void(*)(void *, enum sol_platform_state))defaultIntMonitor);
+}
+
+NAN_METHOD(bind_sol_platform_del_state_monitor) {
+	SOL_PLATFORM_MONITOR_DEL(state,
+		(void(*)(void *, enum sol_platform_state))defaultIntMonitor);
+}
+
+NAN_METHOD(bind_sol_platform_add_system_clock_monitor) {
+	SOL_PLATFORM_MONITOR_ADD(system_clock, defaultTimestampMonitor);
+}
+
+NAN_METHOD(bind_sol_platform_del_system_clock_monitor) {
+	SOL_PLATFORM_MONITOR_DEL(system_clock, defaultTimestampMonitor);
 }
