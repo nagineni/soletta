1156d5fec5c1b38283db1e13ffe6e06d66a850cb
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2016-02-07 16:41:51 +0200

    Node.js bindings: Remove duplicate implementation of fillCArray and jsArray
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>

--- bindings/nodejs/src/structures/handles.cc
+++ bindings/nodejs/src/structures/handles.cc
@@ -1,37 +1,9 @@
 #include <nan.h>
 #include "handles.h"
 #include "../common.h"
+#include "../data.h"
 
 using namespace v8;
-
-static Local<Array> jsArrayFromBytes(unsigned char *bytes, size_t length) {
-  size_t index;
-  Local<Array> returnValue = Nan::New<Array>(length);
-
-  for (index = 0; index < length; index++) {
-    Nan::Set(returnValue, index, Nan::New(bytes[index]));
-  }
-  return returnValue;
-}
-
-static bool fillCArrayFromJSArray(unsigned char *bytes, size_t length,
-                                  Local<Array> array) {
-  size_t index, arrayLength;
-
-  arrayLength = array->Length();
-  if (arrayLength != length) {
-    Nan::ThrowError("byte array has the wrong length");
-    return false;
-  }
-
-  for (index = 0; index < length; index++) {
-    Local<Value> byte = Nan::Get(array, index).ToLocalChecked();
-    VALIDATE_VALUE_TYPE(byte, IsUint32, "byte array item", false);
-    bytes[index] = (unsigned char)(byte->Uint32Value());
-  }
-
-  return true;
-}
 
 #define C_HANDLE(jsHandle, cType, destination)                         \
   cType local;                                                         \
