db3fbd77aa38add9803e47962e32c6a20f29fff1
Sudarsana Nagineni <sudarsana.nagineni@intel.com>
2016-02-16 19:47:30 +0200

    Node.js bindings: Add missing pwm interfaces.
    Add missing implementation of sol_pwm_open_by_label
    and sol_pwm_open_raw APIs.
    
    Signed-off-by: Sudarsana Nagineni <sudarsana.nagineni@intel.com>

--- bindings/nodejs/src/functions/pwm.cc
+++ bindings/nodejs/src/functions/pwm.cc
@@ -1,142 +1,218 @@
-extern "C" {
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <sol-pwm.h>
-}
 
 #include <v8.h>
 #include <node.h>
 #include <nan.h>
 
 #include "../common.h"
-#include "../structures/sol-js-pwm.h"
 #include "../structures/handles.h"
+#include "../structures/sol-js-pwm.h"
 
 using namespace v8;
 
 NAN_METHOD(bind_sol_pwm_open) {
-	VALIDATE_ARGUMENT_COUNT(info, 3);
-	VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsInt32);
- 	VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 1, IsInt32);
-	VALIDATE_ARGUMENT_TYPE(info, 2, IsObject);
+    VALIDATE_ARGUMENT_COUNT(info, 3);
+    VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsInt32);
+    VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 1, IsInt32);
+    VALIDATE_ARGUMENT_TYPE(info, 2, IsObject);
 
-	int device;
-  int channel;
-	sol_pwm_config config;
-	sol_pwm *pwm = NULL;
+    int device;
+    int channel;
+    sol_pwm_config config;
+    sol_pwm *pwm = NULL;
 
-	device = info[0]->Int32Value();
-  channel = info[1]->Int32Value();
+    device = info[0]->Int32Value();
+    channel = info[1]->Int32Value();
 
-	if (!c_sol_pwm_config(info[2]->ToObject(), &config)) {
-		printf("Unable to extract sol_pwm_config\n");
-    	return;
-	}
+    if (!c_sol_pwm_config(info[2]->ToObject(), &config)) {
+        Nan::ThrowError("Unable to extract sol_pwm_config\n");
+        return;
+    }
 
-	pwm = sol_pwm_open(device, channel, &config);
-	if ( pwm ) {
-		info.GetReturnValue().Set(js_sol_pwm(pwm));
-	}
+    pwm = sol_pwm_open(device, channel, &config);
+    if ( pwm ) {
+        info.GetReturnValue().Set(js_sol_pwm(pwm));
+    }
+}
+
+NAN_METHOD(bind_sol_pwm_open_raw) {
+    VALIDATE_ARGUMENT_COUNT(info, 3);
+    VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsInt32);
+    VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 1, IsInt32);
+    VALIDATE_ARGUMENT_TYPE(info, 2, IsObject);
+
+    int device;
+    int channel;
+    sol_pwm_config config;
+    sol_pwm *pwm = NULL;
+
+    device = info[0]->Int32Value();
+    channel = info[1]->Int32Value();
+
+    if (!c_sol_pwm_config(info[2]->ToObject(), &config)) {
+        Nan::ThrowError("Unable to extract sol_pwm_config\n");
+        return;
+    }
+
+    pwm = sol_pwm_open_raw(device, channel, &config);
+    if ( pwm ) {
+        info.GetReturnValue().Set(js_sol_pwm(pwm));
+    }
+}
+
+NAN_METHOD(bind_sol_pwm_open_by_label) {
+    VALIDATE_ARGUMENT_COUNT(info, 2);
+    VALIDATE_ARGUMENT_TYPE_OR_NULL(info, 0, IsString);
+    VALIDATE_ARGUMENT_TYPE(info, 1, IsObject);
+
+    sol_pwm_config config;
+    sol_pwm *pwm = NULL;
+
+    if (!c_sol_pwm_config(info[1]->ToObject(), &config)) {
+        Nan::ThrowError("Unable to extract sol_pwm_config\n");
+        return;
+    }
+
+    pwm = sol_pwm_open_by_label((const char *)*String::Utf8Value(info[0]),
+                                &config);
+
+    if ( pwm ) {
+        info.GetReturnValue().Set(js_sol_pwm(pwm));
+    }
 }
 
 NAN_METHOD(bind_sol_pwm_close) {
-	VALIDATE_ARGUMENT_COUNT(info, 1);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_COUNT(info, 1);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
 
-	sol_pwm *pwm = NULL;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	sol_pwm_close(pwm);
+    sol_pwm_close(pwm);
 }
 
 NAN_METHOD(bind_sol_pwm_set_enabled) {
-	VALIDATE_ARGUMENT_COUNT(info, 2);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsBoolean);
+    VALIDATE_ARGUMENT_COUNT(info, 2);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_TYPE(info, 1, IsBoolean);
 
-	bool value;
-	sol_pwm *pwm = NULL;
+    bool value;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	value = info[1]->BooleanValue();
+    value = info[1]->BooleanValue();
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_set_enabled(pwm, value)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_set_enabled(pwm, value)));
 }
 
 NAN_METHOD(bind_sol_pwm_set_period) {
-	VALIDATE_ARGUMENT_COUNT(info, 2);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsUint32);
+    VALIDATE_ARGUMENT_COUNT(info, 2);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_TYPE(info, 1, IsUint32);
 
-	uint32_t value;
-	sol_pwm *pwm = NULL;
+    uint32_t value;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	value = info[1]->Uint32Value();
+    value = info[1]->Uint32Value();
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_set_period(pwm, value)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_set_period(pwm, value)));
 }
 
 NAN_METHOD(bind_sol_pwm_set_duty_cycle) {
-	VALIDATE_ARGUMENT_COUNT(info, 2);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsUint32);
+    VALIDATE_ARGUMENT_COUNT(info, 2);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_TYPE(info, 1, IsUint32);
 
-	uint32_t value;
-	sol_pwm *pwm = NULL;
+    uint32_t value;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	value = info[1]->Uint32Value();
+    value = info[1]->Uint32Value();
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_set_duty_cycle(pwm, value)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_set_duty_cycle(pwm, value)));
 }
 
 
 NAN_METHOD(bind_sol_pwm_get_enabled) {
-	VALIDATE_ARGUMENT_COUNT(info, 1);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_COUNT(info, 1);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
 
-	sol_pwm *pwm = NULL;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_get_enabled(pwm)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_get_enabled(pwm)));
 }
 
 NAN_METHOD(bind_sol_pwm_get_period) {
-	VALIDATE_ARGUMENT_COUNT(info, 1);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_COUNT(info, 1);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
 
-	sol_pwm *pwm = NULL;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_get_period(pwm)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_get_period(pwm)));
 }
 
 NAN_METHOD(bind_sol_pwm_get_duty_cycle) {
-	VALIDATE_ARGUMENT_COUNT(info, 1);
-	VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
+    VALIDATE_ARGUMENT_COUNT(info, 1);
+    VALIDATE_ARGUMENT_TYPE(info, 0, IsArray);
 
-	sol_pwm *pwm = NULL;
+    sol_pwm *pwm = NULL;
 
- 	if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
+    if (!c_sol_pwm(Local<Array>::Cast(info[0]), &pwm)) {
     	return;
-	}
+    }
 
-	info.GetReturnValue().Set(Nan::New(sol_pwm_get_duty_cycle(pwm)));
+    info.GetReturnValue().Set(Nan::New(sol_pwm_get_duty_cycle(pwm)));
 }
--- bindings/nodejs/src/structures/sol-js-pwm.cc
+++ bindings/nodejs/src/structures/sol-js-pwm.cc
@@ -1,44 +1,70 @@
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include <nan.h>
 #include "sol-js-pwm.h"
 #include "../common.h"
 
 using namespace v8;
 
-extern "C" {
-#include <string.h>
-}
-
-
 bool c_sol_pwm_config(v8::Local<v8::Object> jsPWMConfig, sol_pwm_config *p_config) {
-	sol_pwm_config config = {0, 0, 0, SOL_PWM_ALIGNMENT_LEFT, 
-                    SOL_PWM_POLARITY_NORMAL, false };
+    sol_pwm_config config = {
+#ifdef SOL_PWM_CONFIG_API_VERSION
+        SOL_PWM_CONFIG_API_VERSION,
+#endif /* def SOL_PWM_CONFIG_API_VERSION */
+        0, 0, SOL_PWM_ALIGNMENT_LEFT,
+        SOL_PWM_POLARITY_NORMAL, false };
 
-	VALIDATE_AND_ASSIGN(config, alignment,  sol_pwm_alignment, IsUint32,
-	                	"(PWM Alignment)", false, jsPWMConfig,
-	                	Uint32Value);
+    VALIDATE_AND_ASSIGN(config, alignment,  sol_pwm_alignment, IsUint32,
+                        "(PWM Alignment)", false, jsPWMConfig,
+                        Uint32Value);
 
-  VALIDATE_AND_ASSIGN(config, api_version,  uint16_t, IsUint32,
-	                	"(PWM API Version)", false, jsPWMConfig,
-	                	Uint32Value);
+    VALIDATE_AND_ASSIGN(config, duty_cycle_ns,  uint32_t, IsUint32,
+                        "(PWM Duty Cycle)", false, jsPWMConfig,
+                        Uint32Value);
 
-  VALIDATE_AND_ASSIGN(config, duty_cycle_ns,  uint32_t, IsUint32,
-	                	"(PWM Duty Cycle)", false, jsPWMConfig,
-	                	Uint32Value);
+    VALIDATE_AND_ASSIGN(config, period_ns,  uint32_t, IsUint32,
+                        "(PWM Period)", false, jsPWMConfig,
+                        Uint32Value);
 
- VALIDATE_AND_ASSIGN(config, period_ns,  uint32_t, IsUint32,
-	                	"(PWM Period)", false, jsPWMConfig,
-	                	Uint32Value);
+    VALIDATE_AND_ASSIGN(config, enabled, bool, IsBoolean,
+                        "(PWM enabled)", false, jsPWMConfig,
+                        BooleanValue);
 
+    VALIDATE_AND_ASSIGN(config, polarity, sol_pwm_polarity, IsUint32,
+                        "(PWM polarity)", false, jsPWMConfig,
+                        Uint32Value);
 
-	VALIDATE_AND_ASSIGN(config, enabled, bool, IsBoolean,
-	                	"(PWM enabled)", false, jsPWMConfig,
-	                	BooleanValue);
+    memcpy(p_config, &config, sizeof(sol_pwm_config));
 
-	VALIDATE_AND_ASSIGN(config, polarity, sol_pwm_polarity, IsUint32,
-	                	"(PWM polarity)", false, jsPWMConfig,
-	                	Uint32Value);
-
-	memcpy(p_config, &config, sizeof(sol_pwm_config));
-
-	return true;
+    return true;
 }
