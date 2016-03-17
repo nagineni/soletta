5e48d8dff6ba56f1ced19589b0e4ca327e8ccf24
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2016-03-17 11:35:01 +0530

    Node.js Bindings: Add license and fix indents.

--- bindings/nodejs/src/structures/handles.cc
+++ bindings/nodejs/src/structures/handles.cc
@@ -1,3 +1,21 @@
+/*
+ * This file is part of the Soletta Project
+ *
+ * Copyright (C) 2015 Intel Corporation. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <nan.h>
 #include "handles.h"
 #include "../common.h"
@@ -5,47 +23,47 @@
 
 using namespace v8;
 
-#define C_HANDLE(jsHandle, cType, destination)                         \
-  cType local;                                                         \
-  if (!fillCArrayFromJSArray(((unsigned char *)&local), sizeof(cType), \
-                             jsHandle)) {                              \
-    return false;                                                      \
-  }                                                                    \
-  *destination = local;                                                \
-  return true;
+#define C_HANDLE(jsHandle, cType, destination)  \
+    cType local;    \
+    if (!fillCArrayFromJSArray(((unsigned char *)&local), sizeof(cType),    \
+                             jsHandle)) {   \
+        return false;   \
+    }   \
+    *destination = local;   \
+    return true;
 
 Local<Array> js_sol_aio(sol_aio *handle) {
-  return jsArrayFromBytes(((unsigned char *)(&handle)),
-                          sizeof(sol_aio *));
+    return jsArrayFromBytes(((unsigned char *)(&handle)),
+                            sizeof(sol_aio *));
 }
 
 bool c_sol_aio(Local<Array> handle, sol_aio **p_cHandle) {
-  C_HANDLE(handle, sol_aio *, p_cHandle);
+    C_HANDLE(handle, sol_aio *, p_cHandle);
 }
 
 Local<Array> js_sol_aio_pending(sol_aio_pending *handle) {
-  return jsArrayFromBytes(((unsigned char *)(&handle)),
-                          sizeof(sol_aio_pending *));
+    return jsArrayFromBytes(((unsigned char *)(&handle)),
+                            sizeof(sol_aio_pending *));
 }
 
 bool c_sol_aio_pending(Local<Array> handle, sol_aio_pending **p_cHandle) {
-  C_HANDLE(handle, sol_aio_pending *, p_cHandle);
+    C_HANDLE(handle, sol_aio_pending *, p_cHandle);
 }
 
 Local<Array> js_sol_gpio(sol_gpio *handle) {
-  return jsArrayFromBytes(((unsigned char *)(&handle)),
-                          sizeof(sol_gpio *));
+    return jsArrayFromBytes(((unsigned char *)(&handle)),
+                            sizeof(sol_gpio *));
 }
 
 bool c_sol_gpio(Local<Array> handle, sol_gpio **p_cHandle) {
-  C_HANDLE(handle, sol_gpio *, p_cHandle);
+    C_HANDLE(handle, sol_gpio *, p_cHandle);
 }
 
 Local<Array> js_sol_pwm(sol_pwm *handle) {
-  return jsArrayFromBytes(((unsigned char *)(&handle)),
+    return jsArrayFromBytes(((unsigned char *)(&handle)),
                           sizeof(sol_pwm *));
 }
 
 bool c_sol_pwm(Local<Array> handle, sol_pwm **p_cHandle) {
-  C_HANDLE(handle, sol_pwm *, p_cHandle);
+    C_HANDLE(handle, sol_pwm *, p_cHandle);
 }
