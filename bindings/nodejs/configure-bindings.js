56a8dd00a527c0d4592ba44c80e0cca9fd1b2934
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2016-04-04 21:06:34 +0530

    Build fix.

--- bindings/nodejs/configure-bindings.js
+++ bindings/nodejs/configure-bindings.js
@@ -26,17 +26,14 @@ var sources = [
 	"../src/data.cc",
 	"../src/functions/sol-platform-monitors.cc",
 	"../src/functions/simple.cc",
-	"../src/functions/gpio.cc",
 	"../src/hijack.cc",
 	"../src/sol-uv-integration.c",
-	"../src/structures/js-handle.cc",
-	"../src/structures/sol-js-gpio.cc"
+	"../src/structures/js-handle.cc"
 ];
 
 // List containing the names of the header files in which to search for constants and enums
 var headers = [
 	"sol-platform.h",
-	"sol-gpio.h"
 ];
 
 var oneVariable, match;
@@ -72,6 +69,15 @@ for ( oneVariable in process.env ) {
 			headers = headers.concat( [
 				"sol-coap.h",
 				"sol-network.h"
+			] );
+			break;
+		case "USE_GPIO":
+			sources = sources.concat( [
+				"../src/functions/gpio.cc",
+				"../src/structures/sol-js-gpio.cc"
+			] );
+			headers = headers.concat( [
+				"sol-gpio.h"
 			] );
 			break;
 		default:
