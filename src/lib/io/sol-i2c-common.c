46d1144cb7246834b16a20ddaac9b70d7914c39f
Srinivasa Ragavan <srinivasa.ragavan.venkateswaran@intel.com>
2016-04-13 18:33:38 +0530

    Some build fixes.

--- src/lib/io/sol-i2c-common.c
+++ src/lib/io/sol-i2c-common.c
@@ -27,6 +27,7 @@ SOL_LOG_INTERNAL_DECLARE_STATIC(_log_domain, "i2c");
 #include "sol-pin-mux.h"
 #endif
 #include "sol-str-table.h"
+#include "sol-util.h"
 #include "sol-vector.h"
 
 struct sol_i2c_shared {
